77714e78c2af8debd597d33515bb73fa80458aca
CruzanCaramele <letters2hamza@gmail.com>
2016-09-07 21:12:40 +0800

    remove duplicate directories and replace vhost.d with the default.d for virtual hosts configurations

---
+++ nginx/manifests/params.pp
@@ -0,0 +1,17 @@
+# Class: ngixn::params
+#
+#
+class nginx::params {
+	# resources
+	$package_name           = "nginx"
+	$service_name           = "nginx"
+	$app_root_www           = "/var/www/myapp/code"
+	$error_log_dir          = "/var/log/nginx"
+	$access_log_dir         = "/var/log/nginx"
+	$vhost_config_dir       = "/etc/nginx/conf.d"
+	$passenger_package      = "passenger"
+	$yum_utils_package      = "yum-utils"
+	$nginx_config_dir       = "/etc/nginx"
+	$passenger_config_dir   = "/etc/nginx/conf.d"
+	$epel_release_package   = "epel-release"
+}
