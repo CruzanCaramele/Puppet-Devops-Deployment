7853eb9e0794effa2ff141f32d9e18149b829696
CruzanCaramele <letters2hamza@gmail.com>
2016-09-07 21:11:53 +0800

    install passenger after nginx

---
+++ nginx/manifests/package.pp
@@ -0,0 +1,44 @@
+# Class: nginx::package
+#
+#
+class nginx::package (
+
+	$package_name         = $nginx::params::package_name,
+	$passenger_package    = $nginx::params::passenger_package,
+	$epel_release_package = $nginx::params::epel_release_package,
+	$yum_utils_package    = $nginx::params::yum_package
+
+	){
+
+	$pre_requisites = ['pygpgme', 'curl']
+
+	# resources
+	package { 'epel-release':
+		name   => $epel_release_package,
+		ensure => installed,
+		before => [Package['nginx'], Package['yum-utils'],Package[$pre_requisites]],
+	}
+
+	package { 'yum-utils':
+		name   => $yum_utils_package,
+		ensure => installed,
+		before => [Package['nginx'], Package[$pre_requisites]],
+	}
+
+	package { $pre_requisites:
+		ensure  => installed,
+		require => Package['epel-release'],
+		before  => Package['nginx'],
+	}
+
+	package { 'nginx':
+	    name   => $package_name,
+		ensure => installed,
+	}
+
+	package { 'passenger':
+		name    => $passenger_package,
+		ensure  => installed,
+		require => Package['nginx'],
+	}
+}
