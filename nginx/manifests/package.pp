f394520d8a4a6b21ddcf53d0f7ecb554d6fb4ed6
CruzanCaramele <letters2hamza@gmail.com>
2016-09-11 21:46:24 +0800

    remove duplicate installation of epel-release

--- nginx/manifests/package.pp
+++ nginx/manifests/package.pp
@@ -5,19 +5,18 @@ class nginx::package (
 
 	$package_name         = $nginx::params::package_name,
 	$passenger_package    = $nginx::params::passenger_package,
-	$epel_release_package = $nginx::params::epel_release_package,
 	$yum_utils_package    = $nginx::params::yum_package
 
 	){
 
-	$pre_requisites = ['pygpgme', 'curl', 'git']
+	$pre_requisites = ['pygpgme', 'git']
 
 	# resources
-	package { 'epel-release':
-		name   => $epel_release_package,
-		ensure => installed,
-		before => [Package['nginx'], Package['yum-utils'],Package[$pre_requisites]],
-	}
+	# package { 'epel-release':
+	# 	name   => $epel_release_package,
+	# 	ensure => installed,
+	# 	before => [Package['nginx'], Package['yum-utils'],Package[$pre_requisites]],
+	# }
 
 	package { 'yum-utils':
 		name   => $yum_utils_package,
@@ -27,7 +26,7 @@ class nginx::package (
 
 	package { $pre_requisites:
 		ensure  => installed,
-		require => Package['epel-release'],
+		#require => Package['epel-release'],
 		before  => Package['nginx'],
 	}
 
