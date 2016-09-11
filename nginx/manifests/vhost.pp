057310dbbd743ab94f532827ea5ae01e0beedc4b
CruzanCaramele <letters2hamza@gmail.com>
2016-09-11 16:03:24 +0800

    notify service nginx upon changes to config files

--- nginx/manifests/vhost.pp
+++ nginx/manifests/vhost.pp
@@ -13,19 +13,13 @@ define nginx::vhost (
 			'/var/www/myapp',
 			'/var/www/myapp/code']:
 		ensure => directory,
-		before => File['/var/www/myapp/code/index-html'],
-	}
-
-	file { '/var/www/myapp/code/index-html':
-		ensure => file,
-		source => 'puppet:///modules/nginx/index.html',
-		before => File['nginx-conf']
 	}
 
 	file { 'nginx-conf':
 		ensure => file,
 		path   => "${nginx_config_dir}/nginx.conf",
 		source => 'puppet:///modules/nginx/nginx.conf',
+		notify => Service['nginx'],
 		before => [File['passenger-conf'], File['app-conf']],
 	}
 
@@ -33,25 +27,30 @@ define nginx::vhost (
 		ensure  => file,
 		path    => "${passenger_config_dir}/passenger.conf",
 		source  => 'puppet:///modules/nginx/passenger.conf',
+		notify  => Service['nginx'],
 		before  => File['app-conf'],
 	}
 
 	file { 'app-conf':
-		ensure  => file,
-		path    => "${vhost_config_dir}/app.conf",
-		source  => 'puppet:///modules/nginx/app.conf',
+		ensure   => file,
+		path     => "${vhost_config_dir}/app.conf",
+		notify   => Service['nginx'],
+		content  => template('nginx/app.conf.erb'),
 	}
 
-	vcsrepo {$app_root_www
-		ensure   => present,
-		provider => git,
-		source   => 'https://github.com/phusion/passenger-nodejs-connect-demo.git',
+	vcsrepo {$app_root_www:
+		ensure    => present,
+		provider  => git,
+		revision  => master,
+		force     => true,
+		notify    => Service['nginx'],
+		source    => 'https://github.com/phusion/passenger-nodejs-connect-demo.git',
 	}
 
 	exec { 'run-app':
 		command      => 'npm install --production',
-		path        => '/usr/bin:/usr/sbin:/bin:/usr/local/bin',
-		cwd         => $app_root_www,
+		path         => '/usr/bin:/usr/sbin:/bin:/usr/local/bin',
+		cwd          => $app_root_www,
 		#user        => 'user_to_run_as',
 		#unless      => 'test param-that-would-be-true',
 		#refreshonly => true,
