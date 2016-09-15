5373029090337b2710e7eaeb9240a3c6e2b4f548
CruzanCaramele <letters2hamza@gmail.com>
2016-09-15 22:44:37 +0800

    class apache declartion

--- apache/manifests/init.pp
+++ apache/manifests/init.pp
@@ -1,41 +1,34 @@
 # == Class: apache
-#
-# Full description of class apache here.
-#
-# === Parameters
-#
-# Document parameters here.
-#
-# [*sample_parameter*]
-#   Explanation of what this parameter affects and what it defaults to.
-#   e.g. "Specify one or more upstream ntp servers as an array."
-#
-# === Variables
-#
-# Here you should define a list of variables that this module would require.
-#
-# [*sample_variable*]
-#   Explanation of how this variable affects the funtion of this class and if
-#   it has a default. e.g. "The parameter enc_ntp_servers must be set by the
-#   External Node Classifier as a comma separated list of hostnames." (Note,
-#   global variables should be avoided in favor of class parameters as
-#   of Puppet 2.6.)
-#
-# === Examples
-#
-#  class { 'apache':
-#    servers => [ 'pool.ntp.org', 'ntp.local.company.com' ],
-#  }
-#
 # === Authors
 #
-# Author Name <author@domain.com>
+# Hamza Yahaya <letters2hamza@gmail.com>
 #
 # === Copyright
 #
-# Copyright 2016 Your name here, unless otherwise noted.
+# Copyright 2016 Hamza Yahaya
 #
-class apache {
+class apache (
 
+	$package_name = $apache::params::package_name,
+	$service_name = $apache::params::service_name
 
+	) inherits apache::params {
+
+	include apache::service
+	include apache::package
+
+	exec { 'enable-epel':
+		command      => 'yum-config-manager --enable epel',
+		path         => '/usr/bin:/usr/sbin:/bin:/usr/local/bin',
+		require      => Package["epel-release"],
+		#unless      => 'test param-that-would-be-true',\
+	} ->
+
+	exec { 'add-el7-repo':
+		command      => 'curl --fail -sSLo /etc/yum.repos.d/passenger.repo https://oss-binaries.phusionpassenger.com/yum/definitions/el-passenger.repo',
+		path         => '/usr/bin:/usr/sbin:/bin:/usr/local/bin',
+		require      => [Package['yum-utils'],Package[$apache::package::pre_requisites]],
+		before       => Package['apache'],
+		#unless      => 'test param-that-would-be-true',
+	}
 }
