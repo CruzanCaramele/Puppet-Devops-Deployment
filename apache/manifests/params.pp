70c1c532d96970ffdfb16a26027b1a64d283b1fa
CruzanCaramele <letters2hamza@gmail.com>
2016-09-18 00:53:51 +0800

    make wsgi and app roo directory the same

--- apache/manifests/params.pp
+++ apache/manifests/params.pp
@@ -10,8 +10,12 @@ class apache::params {
 	}
 
 	# resources
-	$package_name         = "mod_passenger"
+	$package_name         = "mod_wsgi"
 	$service_name         = "httpd"
+	$config_dir           = "/etc/http/conf"
+	$app_directory        = "/var/www/myapp"
+	$app_config_dir       = "/etc/httpd/conf.d"
+	$wsgi_config_dir      = "/var/www/myapp"
 	$yum_utils_package    = "yum-utils"
 	$epel_release_package = "epel-release"
 }
