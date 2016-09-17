34b584fc9b5df624e74ab16cc1b3a7997c20c237
CruzanCaramele <letters2hamza@gmail.com>
2016-09-18 00:53:06 +0800

    remove passenger repo

--- apache/manifests/init.pp
+++ apache/manifests/init.pp
@@ -9,13 +9,17 @@
 #
 class apache (
 
-	$package_name = $apache::params::package_name,
-	$service_name = $apache::params::service_name
+	$package_name    = $apache::params::package_name,
+	$service_name    = $apache::params::service_name,
+	$app_directory   = $apache::params::app_directory,
+	$app_config_dir  = $apache::params::app_config_dir,
+	$wsgi_config_dir = $apache::params::wsgi_config_dir 
+
 
 	) inherits apache::params {
 
-	include apache::service
 	include apache::package
+	include apache::service
 
 	exec { 'enable-epel':
 		command      => 'yum-config-manager --enable epel',
@@ -24,11 +28,17 @@ class apache (
 		#unless      => 'test param-that-would-be-true',\
 	} ->
 
-	exec { 'add-el7-repo':
-		command      => 'curl --fail -sSLo /etc/yum.repos.d/passenger.repo https://oss-binaries.phusionpassenger.com/yum/definitions/el-passenger.repo',
-		path         => '/usr/bin:/usr/sbin:/bin:/usr/local/bin',
-		require      => [Package['yum-utils'],Package[$apache::package::pre_requisites]],
-		before       => Package['apache'],
-		#unless      => 'test param-that-would-be-true',
+	# exec { 'add-el7-repo':
+	# 	command      => 'curl --fail -sSLo /etc/yum.repos.d/passenger.repo https://oss-binaries.phusionpassenger.com/yum/definitions/el-passenger.repo',
+	# 	path         => '/usr/bin:/usr/sbin:/bin:/usr/local/bin',
+	# 	require      => [Package['yum-utils'],Package[$apache::package::pre_requisites]],
+	# 	before       => Package['apache'],
+	# 	#unless      => 'test param-that-would-be-true',
+	# }
+
+	apache::vhost {"default":
+		app_directory   => $app_directory,
+		app_config_dir  => $app_config_dir,
+		wsgi_config_dir => $wsgi_config_dir,
 	}
 }
