fb38f9fd4eae78d740fb7d6a0db1e97186b126ea
CruzanCaramele <letters2hamza@gmail.com>
2017-06-03 21:37:58 +0800

    add pid file location and log directory parameters

--- nginx-basic/manifests/params.pp
+++ nginx-basic/manifests/params.pp
@@ -2,12 +2,14 @@
 #
 class nginx::params {
 	# parameters
-	$ensure        = 'present'
-	$config_dir    = '/etc/nginx'
-	$config_mode   = '0664'
-	$config_group  = 'root'
-	$config_owner  = 'root'
-	$package_name  = 'nginx'
-	$config_confd  = "${config_dir}/conf.d"
-	$config_ensure = 'file'
+	$ensure          = 'present'
+	$config_dir      = '/etc/nginx'
+	$config_mode     = '0664'
+	$config_group    = 'root'
+	$config_owner    = 'root'
+	$package_name    = 'nginx'
+	$config_confd    = "${config_dir}/conf.d"
+	$config_ensure   = 'file'
+	$config_log_dir  = '/var/log/nginx'
+	$config_pid_file = '/run/nginx.pid'
 }
