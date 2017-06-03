8fa9f3a91ff30ce4f2b0896c7eed74cc83bc886a
CruzanCaramele <letters2hamza@gmail.com>
2017-06-04 01:07:34 +0800

    use empty value for server aliases parameter

--- nginx-basic/manifests/vhost.pp
+++ nginx-basic/manifests/vhost.pp
@@ -7,7 +7,7 @@ define nginx::vhost (
 	String  $config_mode				 = $::nginx::config_mode,
 	String  $config_group				 = $::nginx::config_group,
 	String  $config_owner				 = $::nginx::config_owner,
-	String  $server_aliases				 = 
+	String  $server_aliases				 = '',
 	String  $config_log_dir				 = $::nginx::config_log_dir,				 
 	String  $config_vhost_dir            = $::nginx::config_vhost_dir,
 	Optional[String] $config_vdir_enable = $::nginx::config_vdir_enable,
