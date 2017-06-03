978519ece158cbe7befd01a078246e5897384b07
CruzanCaramele <letters2hamza@gmail.com>
2017-06-03 23:26:00 +0800

    add optional parameter

--- nginx-basic/manifests/params.pp
+++ nginx-basic/manifests/params.pp
@@ -28,7 +28,7 @@ class nginx::params {
 
 	$vhost_dir = $facts['os']['family'] ? {
 		'Debian' => "${config_dir}/sites-enabled",
-		default  => ${config_confd},
+		default  => $config_confd,
 	}
 
 	# service parameters
